c9d46f1192a8308e026762ef18efa74843ec559e
Satyam Thakur <sat.thakur20@gmail.com>
2024-01-22 14:03:35 +0530

    Creating EditNumberField with an amountInput state

--- app/src/main/java/com/satyamthakur/learning/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/tipcalculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.satyamthakur.learning.tipcalculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
@@ -16,7 +18,11 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -50,6 +56,21 @@ fun TipTimeLayoutPreview() {
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun EditNumberField(
+    modifier: Modifier = Modifier
+) {
+    var amountInput: MutableState<String> = remember { mutableStateOf("0") }
+    TextField(
+        value = amountInput.value,
+        onValueChange = {
+            amountInput.value = it
+        },
+        modifier = modifier
+    )
+}
+
 
 @Composable
 fun TipTimeLayout() {
@@ -68,6 +89,13 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
+
+        EditNumberField(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
+
         Text(
             text = stringResource(R.string.tip_amount, "$0.00"),
             style = MaterialTheme.typography.displaySmall
--- app/src/main/java/com/satyamthakur/learning/tipcalculator/ui/theme/Color.kt
+++ app/src/main/java/com/satyamthakur/learning/tipcalculator/ui/theme/Color.kt
@@ -61,11 +61,3 @@ val md_theme_dark_inversePrimary = Color(0xFF984061)
 val md_theme_dark_surfaceTint = Color(0xFFFFB0C8)
 val md_theme_dark_outlineVariant = Color(0xFF514347)
 val md_theme_dark_scrim = Color(0xFF000000)
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
