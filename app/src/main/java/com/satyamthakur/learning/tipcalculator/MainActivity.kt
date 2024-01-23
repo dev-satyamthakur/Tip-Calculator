dc50be39167e0e8fa2b66157ada06809161019fe
Satyam Thakur <sat.thakur20@gmail.com>
2024-01-23 18:28:26 +0530

    Hoisting state and showing calculated tip

--- app/src/main/java/com/satyamthakur/learning/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/tipcalculator/MainActivity.kt
@@ -22,8 +22,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -61,14 +63,13 @@ fun TipTimeLayoutPreview() {
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var amountInput: MutableState<String> = remember { mutableStateOf("0") }
     TextField(
-        value = amountInput.value,
-        onValueChange = {
-            amountInput.value = it
-        },
+        value = value,
+        onValueChange = onValueChange,
         modifier = modifier,
         label = { Text(text = stringResource(id = R.string.bill_amount)) },
         singleLine = true,
@@ -79,6 +80,11 @@ fun EditNumberField(
 
 @Composable
 fun TipTimeLayout() {
+
+    var amountInput by remember { mutableStateOf("0") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -96,13 +102,14 @@ fun TipTimeLayout() {
         )
 
         EditNumberField(
+            value = amountInput,
+            onValueChange = { amountInput = it },
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
         )
-
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
